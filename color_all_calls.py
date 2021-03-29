89927da736da453170e86ca3e784c132f2e58464
HarshilPatel007 <p.h.hacker007@gmail.com>
2021-03-29 15:12:52 +0530

    first initial commit

---
+++ color_all_calls.py
@@ -0,0 +1,25 @@
+# Color(Green) all Call(s) in flow type.
+# https://ghidra.re/ghidra_docs/api/ghidra/program/model/symbol/FlowType.html#isCall()
+# @author : Harshil Patel
+# @category : Tools
+
+from java.awt import Color
+
+
+call_count = 0
+CALL_COLOR = Color(190, 255, 143) # Green
+
+#get all memory ranges
+addr_ranges = currentProgram.getMemory().getAddressRanges()
+
+for addr_range in addr_ranges:
+    insts = currentProgram.getListing().getInstructions(addr_range.getMinAddress(), True)
+    for inst in insts:
+        flow_type = inst.getFlowType()
+        addr = inst.getAddress()
+        if flow_type.isCall():
+            print("0x{} : {}".format(addr, inst))
+            setBackgroundColor(addr, CALL_COLOR)
+            call_count += 1
+
+print('[*] Total Call(s) : {}'.format(call_count))
---
+++ color_all_jumps.py
@@ -0,0 +1,25 @@
+# Color(Red) all Jump(s) in flow type.
+# https://ghidra.re/ghidra_docs/api/ghidra/program/model/symbol/FlowType.html#isJump()
+# @author : Harshil Patel
+# @category : Tools
+
+from java.awt import Color
+
+
+jump_count = 0
+JUMP_COLOR = Color(255, 143, 143) # Red
+
+#get all memory ranges
+addr_ranges = currentProgram.getMemory().getAddressRanges()
+
+for addr_range in addr_ranges:
+    insts = currentProgram.getListing().getInstructions(addr_range.getMinAddress(), True)
+    for inst in insts:
+        flow_type = inst.getFlowType()
+        addr = inst.getAddress()
+        if flow_type.isJump():
+            print("0x{} : {}".format(addr, inst))
+            setBackgroundColor(addr, JUMP_COLOR)
+            jump_count += 1
+
+print('[*] Total Jump(s) : {}'.format(jump_count))
